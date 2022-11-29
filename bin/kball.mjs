d59e8e185ae39ae6665d3eeffee60ab67f58325f
Brian C Sparks <briancsparks@gmail.com>
2022-11-29 01:13:38 -0800

    getting vpcs

--- bin/kball.mjs
+++ bin/kball.mjs
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 
-import { $ } from 'zx';
+// import { $ } from 'zx';
+//
+// await $`ls`
 
-await $`ls`
+
+import { getGetVpcs } from '../lib/aws/ec2.mjs';
+import { inspect } from '../lib/utils.mjs';
+
+(async function() {
+  await main();
+})().catch(console.dir);
+
+async function main() {
+
+  const getVpcs = getGetVpcs();
+  console.log(inspect(await getVpcs()));
+
+  console.log('booya');
+}
+
+
