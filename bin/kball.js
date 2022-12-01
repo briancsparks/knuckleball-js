eb0ce13e68cc9aec366dc10f34ba365942e31596
Brian C Sparks <briancsparks@gmail.com>
2022-11-30 21:33:47 -0800

    aws working but wip

--- bin/kball.js
+++ bin/kball.js
@@ -1,15 +1,9 @@
 
-const { getGetVpcs } = require('../lib/aws/ec2');
-const { inspect } = require('../lib/utils');
-
 (async function() {
   await main();
 })().catch(console.dir);
 
 async function main() {
-
-  const getVpcs = getGetVpcs();
-  console.log(inspect(await getVpcs()));
 
   console.log('booya');
 }
